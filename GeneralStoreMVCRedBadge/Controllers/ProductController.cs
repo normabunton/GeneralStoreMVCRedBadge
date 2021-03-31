3f3e50857550c247fc1fc6b9498aa932d4d1f3b8
normabunton <te_nj25@hotmail.com>
2021-03-31 12:46:30 -0400

    commit for controllers

---
+++ GeneralStoreMVCRedBadge/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using GeneralStoreMVCRedBadge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStoreMVCRedBadge.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        //Add the application DB Context (link to tge database)
+
+        private ApplicationDbContext _db = new ApplicationDbContext
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            //(modifying the ApplicaitonDbContext class)
+            return View(_db.Products.ToList();
+        }
+    }
+}
---
+++ GeneralStoreMVCRedBadge/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreMVCRedBadge.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+        [Required, Display(Name = "# In Stock")]
+        public int InventoryCount { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required, Display(Name = "It is Food")]
+        public Boolean IsFood { get; set; }
+    }
+    public class GeneralStoreDbContext : DbContext {
+        public DbSet<Product> Products { get; set; }
+    }
+}
